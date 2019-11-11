9502cf9c011f6a9ead298a7e09f238ae6aa43996
Ubuntu <ubuntu@ip-10-0-1-33.eu-west-2.compute.internal>
2019-11-11 13:06:36 +0000

    change intrall grafana script

--- 3-install-grafana.sh
+++ 3-install-grafana.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo 'deb https://packagecloud.io/grafana/stable/debian/ jessie main' >> /etc/apt/sources.list
-curl https://packagecloud.io/gpg.key 
-sudo apt-key add - gpg.key
+curl https://packagecloud.io/gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install grafana
 
